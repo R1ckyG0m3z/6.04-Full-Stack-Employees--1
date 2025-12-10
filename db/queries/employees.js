0a263fc779d74527f901e0a956cc69c5e4339305
Ricky <gomez.ricky@gmail.com>
2025-12-10 15:31:00 -0600

    Completed FullStack Employee 1

--- db/queries/employees.js
+++ db/queries/employees.js
@@ -1,5 +1,21 @@
-/** @returns the employee created according to the provided details */
+import db from "#db/client";
+
 export async function createEmployee({ name, birthday, salary }) {
+  const sql = `
+  INSERT INTO employees
+    (name, birthday, salary)
+  VALUES
+    ($1, $2, $3)
+  RETURNING *
+  `;
+  const {
+    rows: [employee],
+  } = await db.query(sql, [name, birthday, salary]);
+  return employee;
+}
+
+/** @returns all departments */
+export async function getDepartments() {
   // TODO
 }
 
